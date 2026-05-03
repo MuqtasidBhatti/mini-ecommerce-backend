81299766526377cf37a712cd7bfdf8908e768620
muqtasidbhatti <muqtasidbhatti246@gmail.com>
2026-05-03 05:32:41 +0500

    fix: add duplicate email check on registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,19 +5,22 @@ const jwt = require('jsonwebtoken')
 
 const registerUser = async (req, res) => {
     try {
-
         const { name, email, password } = req.body
 
-        const hashedPassword = await bcrypt.hash(password, 10)
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already in use" })
+        }
 
+        const hashedPassword = await bcrypt.hash(password, 10)
         const user = new User({ name, email, password: hashedPassword })
         await user.save()
         res.status(201).json({ message: "New user created" })
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
-
 }
+
 
 const loginUser = async (req, res) => {
     try {
